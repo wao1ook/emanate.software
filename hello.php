3a07a5225ae4ed08667f0d20d1e2f9f10abcfad7
James E. Msenga <hello@jamesemanuel.me>
2022-11-12 19:23:38 +0300

    Revert "Removing Contact From Website"
    This reverts commit 4554fe265972d33fc6a1f2503ea6003d7a876aca.

---
+++ hello.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+
+<?php include 'inc/head.php'; ?>
+
+<body>
+<?php include 'inc/preloader.php'; ?>
+
+<?php include 'inc/menu.php'; ?>
+
+<header class="page-header">
+    <div class="video-bg">
+        <img src="images/hello.jpg" alt="Emanate Software">
+    </div>
+    <div class="container">
+        <h1>Howdy</h1>
+        <p>TO CREATE A POWERFUL PROJECT ONCE, A BIT OF LUCK IS ENOUGH</p>
+    </div>
+    <?php include 'inc/aside.php' ?>
+</header>
+
+<section class="hello">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 wow fadeIn">
+                <h6>LET'S HAVE COFFEE TOGETHER</h6>
+                <h2 data-text="Howdy">Say hello !!</h2>
+            </div>
+            <div class="col-md-4 wow fadeIn">
+                <address>
+                    <b>Address</b>
+                    <p>Call or email us for an appointment</p>
+                </address>
+            </div>
+            <div class="col-md-4 wow fadeIn" data-wow-delay="0.05s">
+                <address>
+                    <b>Phone</b>
+                    <p> <a href="tel:+255656791558">+255 656 791 558</a> </p>
+                </address>
+            </div>
+            <div class="col-md-4 wow fadeIn" data-wow-delay="0.10s">
+                <address>
+                    <b>Email</b>
+                    <a href="mailto:hello@emanate.software">hello@emanate.software</a>
+                </address>
+            </div>
+        </div>
+
+        <div class="row align-items-center">
+            <div class="col-lg-12 wow fadeIn" data-wow-delay="0.05s">
+                <div id="success" class="alert alert-success" role="alert"> Your message was sent successfully! We will
+                    be in touch as soon as we can.
+                </div>
+                <div id="error" class="alert alert-danger" role="alert"> Something went wrong, try refreshing and
+                    submitting the form again.
+                </div>
+                <form class="row inner" id="contact" name="contact" method="post" action="process.php">
+                    <div class="form-group col-sm-6 col-12">
+                        <label for="first_name"><span>First Name</span></label>
+                        <input type="text" name="first_name" id="first_name" required>
+                    </div>
+                    <div class="form-group col-sm-6 col-12">
+                        <label for="last_name"><span>Last Name</span></label>
+                        <input type="text" name="last_name" id="last_name" required>
+                    </div>
+                    <div class="form-group col-sm-6 col-12">
+                        <label for="email"><span>Email Address</span></label>
+                        <input type="text" name="email" id="email" required>
+                    </div>
+                    <div class="form-group col-sm-6 col-12">
+                        <label for="subject"><span>Subject</span></label>
+                        <input type="text" name="subject" id="subject" required>
+                    </div>
+                    <div class="form-group col-12">
+                        <label for="message"><span>Your message</span></label>
+                        <textarea name="message" id="message" required></textarea>
+                    </div>
+                    <div class="form-group col-12">
+                        <button id="submit" type="submit" name="submit">SUBMIT</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'inc/footer.php'; ?>
+
+<?php include 'inc/scripts.php' ?>
+</body>
+</html>
--- inc/menu.php
+++ inc/menu.php
@@ -9,6 +9,7 @@
         <div class="menu">
             <ul>
                 <li><a href="../emanate.php">Who are we?</a></li>
+                <li><a href="../hello.php">Get in touch</a></li>
             </ul>
         </div>
         <blockquote>Let's turn your idea into a reality</blockquote>
